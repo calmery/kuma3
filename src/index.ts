831577a04c608d62fb079f75c4f67f3bc354f712
calmery <calmery@users.noreply.github.com>
2022-04-22 15:07:45 +0900

    GitHub へのリンクと誕生日の情報を追加する

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
-import { message } from "./constants";
+import { Command } from "commander";
+import dayjs from "dayjs";
+import open from "open";
 
-console.log(message);
+const program = new Command("kuma3");
+
+program.command("birthday").action(() => {
+  console.log(
+    `1995/12/03 (${dayjs().diff(
+      dayjs("1995/12/03"),
+      "day"
+    )} days have passed since birth.)`
+  );
+});
+
+program.command("github").action(async () => {
+  await open("https://github.com/kuma3");
+});
+
+program.parse();
