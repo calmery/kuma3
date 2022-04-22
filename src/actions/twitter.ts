29a101c4b9ec37c6b2541374fe4f449f6bcf79ce
calmery <calmery@users.noreply.github.com>
2022-04-22 15:43:01 +0900

    feat: Twitter へのリンクを追加する

---
+++ src/actions/twitter.ts
@@ -0,0 +1,5 @@
+import open from "open";
+
+export const twitter = async () => {
+  await open("https://twitter.com/bath_poo_");
+};
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { Command } from "commander";
 import { birthday } from "./actions/birthday";
 import { github } from "./actions/github";
+import { twitter } from "./actions/twitter";
 
 const program = new Command("kuma3");
 
 program.command("birthday").action(birthday);
 program.command("github").action(github);
+program.command("twitter").action(twitter);
 
 program.parse();
